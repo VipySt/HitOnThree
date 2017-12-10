d58aff2f77e5083951dd2223c292b98db7a68d5f
VipY <studio.vipy@gmail.com>
2017-12-10 16:03:01 +0300

    no message

---
+++ HealthBar/AbsHealthBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace ScriptsUnity.HealthBar
+{ 
+    public abstract class AbsHealthBar : MonoBehaviour
+    {
+        public virtual void InitHealthBar(string name, int maxHealth) { }
+        public virtual void SetCountHealth(int health, Color color) { }
+    }
+}
