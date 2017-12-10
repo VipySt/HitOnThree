dc18af315b8e7b5054e7b069117f1ab656f1808a
VipY <studio.vipy@gmail.com>
2017-12-10 19:42:08 +0300

    Change namespace

--- UI_Fight/FightTime/ViewTimeFight.cs
+++ UI_Fight/FightTime/ViewTimeFight.cs
@@ -3,113 +3,117 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ViewTimeFight : MonoBehaviour
+namespace ScriptsUnity.FightTime
 {
-    #region ErrorValues
-    const string _ERR_NULL_TIME_LEFT_VIEW = "Error. Not setted TimeLeftView. Value is null";
-    const string _ERR_NULL_TIME_MASK_LEFT = "Error. Not setted TimeMaskLeft. Value is null";
-    const string _ERR_NULL_TEXT_TIME_VIEW = "Error. Not setted TextTimerView. Value is null";
-    #endregion
 
-    #region Consts Private
-    private const float _MAX_FILL_AMOUNT = 1.0f;
-    private const float _TIMER_ZERO = 0.0f;
-    #endregion
-
-    #region Consts Public
-    public const float NORMAL_MULTY_SPEED_TIMER = 1.0f;
-    #endregion
-
-    public Image TimeLeftView = null;
-    public Image TimeMaskLeft = null;
-    public Text TextTimerView = null;
-    public float MaxTimeRound = 3.0f;
-    public float MultyTimerSpeed = NORMAL_MULTY_SPEED_TIMER;
-
-    public Color ColorBase;
-    public Color ColorActicon;
-
-    private float _timerRound = 0.0f;
-    private bool _working = false;
-
-
-    public void ResetTime()
+    public class ViewTimeFight : MonoBehaviour
     {
-        TimeMaskLeft.gameObject.SetActive(false);
+        #region ErrorValues
+        const string _ERR_NULL_TIME_LEFT_VIEW = "Error. Not setted TimeLeftView. Value is null";
+        const string _ERR_NULL_TIME_MASK_LEFT = "Error. Not setted TimeMaskLeft. Value is null";
+        const string _ERR_NULL_TEXT_TIME_VIEW = "Error. Not setted TextTimerView. Value is null";
+        #endregion
 
-        TimeLeftView.color = ColorBase;
-       
-        _timerRound = _TIMER_ZERO;
-    }
+        #region Consts Private
+        private const float _MAX_FILL_AMOUNT = 1.0f;
+        private const float _TIMER_ZERO = 0.0f;
+        #endregion
 
-    public void StartTimer()
-    {
-        _working = true;
-    }
+        #region Consts Public
+        public const float NORMAL_MULTY_SPEED_TIMER = 1.0f;
+        #endregion
 
-    public void StopTimer()
-    {
-        _working = false;
-    }
-    
+        public Image TimeLeftView = null;
+        public Image TimeMaskLeft = null;
+        public Text TextTimerView = null;
+        public float MaxTimeRound = 3.0f;
+        public float MultyTimerSpeed = NORMAL_MULTY_SPEED_TIMER;
 
-    public float AddActicionTime(float timeAction)
-    {
-        float delta = MaxTimeRound - _timerRound - timeAction;
-        if (delta >= _TIMER_ZERO)
+        public Color ColorBase;
+        public Color ColorActicon;
+
+        private float _timerRound = 0.0f;
+        private bool _working = false;
+
+
+        public void ResetTime()
         {
-            var tScale = delta / MaxTimeRound;
+            TimeMaskLeft.gameObject.SetActive(false);
 
-            TimeLeftView.color = ColorActicon;
+            TimeLeftView.color = ColorBase;
 
-            TimeMaskLeft.gameObject.SetActive(true);
-            TimeMaskLeft.color = ColorBase;
-            TimeMaskLeft.fillAmount = tScale;
+            _timerRound = _TIMER_ZERO;
         }
 
-        return delta;
-    }
-
-
-    private void RecountTimer()
-    {
-        var delta = MaxTimeRound - _timerRound;
-        TimeLeftView.fillAmount = delta / MaxTimeRound;
-        TextTimerView.text = string.Format("End round to {0:F2}", delta);
-
-        if (TimeMaskLeft.gameObject.activeInHierarchy)
+        public void StartTimer()
         {
-            if (TimeMaskLeft.fillAmount < TimeLeftView.fillAmount) { }
-            else
+            _working = true;
+        }
+
+        public void StopTimer()
+        {
+            _working = false;
+        }
+
+
+        public float AddActicionTime(float timeAction)
+        {
+            float delta = MaxTimeRound - _timerRound - timeAction;
+            if (delta >= _TIMER_ZERO)
             {
-                TimeMaskLeft.gameObject.SetActive(false);
-                TimeLeftView.color = ColorBase;
+                var tScale = delta / MaxTimeRound;
+
+                TimeLeftView.color = ColorActicon;
+
+                TimeMaskLeft.gameObject.SetActive(true);
+                TimeMaskLeft.color = ColorBase;
+                TimeMaskLeft.fillAmount = tScale;
             }
+
+            return delta;
         }
 
-        if (delta <= _TIMER_ZERO)
+
+        private void RecountTimer()
         {
-            StopTimer();
-        } //Время рауна вышло
+            var delta = MaxTimeRound - _timerRound;
+            TimeLeftView.fillAmount = delta / MaxTimeRound;
+            TextTimerView.text = string.Format("End round to {0:F2}", delta);
+
+            if (TimeMaskLeft.gameObject.activeInHierarchy)
+            {
+                if (TimeMaskLeft.fillAmount < TimeLeftView.fillAmount) { }
+                else
+                {
+                    TimeMaskLeft.gameObject.SetActive(false);
+                    TimeLeftView.color = ColorBase;
+                }
+            }
+
+            if (delta <= _TIMER_ZERO)
+            {
+                StopTimer();
+            } //Время рауна вышло
+        }
+
+
+        private void Awake()
+        {
+            if (!TimeLeftView)
+                throw new System.ArgumentNullException(_ERR_NULL_TIME_LEFT_VIEW);
+            if (!TimeMaskLeft)
+                throw new System.ArgumentNullException(_ERR_NULL_TIME_MASK_LEFT);
+            if (!TextTimerView)
+                throw new System.ArgumentNullException(_ERR_NULL_TEXT_TIME_VIEW);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_working) return;
+
+            _timerRound += Time.deltaTime + MultyTimerSpeed;
+            RecountTimer();
+        }
+
     }
-
-
-    private void Awake()
-    {
-        if (!TimeLeftView)
-            throw new System.ArgumentNullException(_ERR_NULL_TIME_LEFT_VIEW);
-        if (!TimeMaskLeft)
-            throw new System.ArgumentNullException(_ERR_NULL_TIME_MASK_LEFT);
-        if (!TextTimerView)
-            throw new System.ArgumentNullException(_ERR_NULL_TEXT_TIME_VIEW);
-    }
-
-    private void FixedUpdate()
-    {
-        if (!_working) return;
-
-        _timerRound += Time.deltaTime + MultyTimerSpeed;
-        RecountTimer();
-    }
-
 }
